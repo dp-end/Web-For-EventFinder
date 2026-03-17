a06c053fb770f5122db046082f1d556d356198af
Mert Osman Ayhan <mrtosmanayhan@gmail.com>
2026-03-17 23:30:04 +0300

    frontend dosyası son hali

--- src/app/components/pages/login/login.ts
+++ src/app/components/pages/login/login.ts
@@ -2,6 +2,8 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RouterModule, Router } from '@angular/router';
+import { AuthService } from '../../../services/auth';
+AuthService // Yolu kendi klasör yapına göre ayarla!
 
 @Component({
   selector: 'app-login',
@@ -12,8 +14,13 @@ import { RouterModule, Router } from '@angular/router';
 })
 export class Login {
   loginForm: FormGroup;
+  errorMessage: string = ''; // Backend'den dönen hataları (yanlış şifre vb.) ekranda göstermek için
 
-  constructor(private fb: FormBuilder, private router: Router) {
+  constructor(
+    private fb: FormBuilder,
+    private router: Router,
+    private authService: AuthService // API ile konuşacak garsonumuzu içeri aldık
+  ) {
     this.loginForm = this.fb.group({
       email: ['', [Validators.required, Validators.email]],
       password: ['', [
@@ -25,14 +32,35 @@ export class Login {
   }
 
   onSubmit() {
-    if (this.loginForm.valid) {
-      console.log('Giriş Denemesi:', this.loginForm.value);
+    // Her yeni butona basıldığında eski hatayı ekrandan temizle
+    this.errorMessage = '';
 
-      // ŞİMDİLİK SİMÜLASYON: Form geçerliyse direkt Home sayfasına yönlendir!
-      this.router.navigate(['/home']);
+    if (this.loginForm.valid) {
+      console.log('Giriş Denemesi API ye gönderiliyor...', this.loginForm.value);
+
+      // Gerçek API İsteği:
+      this.authService.login(this.loginForm.value).subscribe({
+        next: (response: any) => {
+          console.log('Giriş Başarılı! Gelen Cevap:', response);
+
+          // API'den gelen JWT Token'ı tarayıcının hafızasına (LocalStorage) kaydediyoruz
+          // Not: Clean Architecture şablonunda token genelde response.data.jwToken içinde döner
+          const token = response.data?.jwToken || response.jwToken;
+          if (token) {
+            localStorage.setItem('token', token);
+          }
+
+          // Giriş başarılıysa Home sayfasına yönlendir!
+          this.router.navigate(['/home']);
+        },
+        error: (err) => {
+          console.error('Backend Hatası:', err);
+          // Kullanıcıya şık bir hata mesajı göster
+          this.errorMessage = 'E-posta veya şifre hatalı. Lütfen tekrar deneyin.';
+        }
+      });
 
     } else {
-      // Form hatalıysa uyarıları göster
       this.loginForm.markAllAsTouched();
     }
   }
